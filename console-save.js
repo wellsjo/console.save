d7c82123e508a97d933f01a59b2e27f7f9c36a15
Wells Johnston <wellsjohnston@gmail.com>
2015-12-05 05:40:31 -0500

    added some comments, better formatting

--- console-save.js
+++ console-save.js
@@ -1,27 +1,43 @@
-(function(console){
+/**
+ * Browser console hack that allows you to save JSON objects to disk.
+ *
+ * @author Wells Johnston
+ */
 
-console.save = function(data, filename){
+(function(console) {
 
-    if(!data) {
-        console.error('Console.save: No data')
-        return;
+  /**
+   * Save json object to disk.
+   *
+   * @param {object} obj The object to save.
+   * @param {string} filename The name of the file to save.
+   */
+  console.save = function(obj, filename) {
+
+    if (!obj) {
+      console.error('console.save: No data.');
+      return;
     }
 
-    if(!filename) filename = 'console.json'
+    if (filename == null) filename = 'console.json';
 
-    if(typeof data === "object"){
-        data = JSON.stringify(data, undefined, 4);
+    if (typeof obj === 'object') {
+      var data = JSON.stringify(obj, undefined, 4);
     }
 
-    var blob = new Blob([data], {type: 'text/json'}),
-        e    = document.createEvent('MouseEvents'),
-        a    = document.createElement('a');
+    // create a phantom link element to download a Blob of data, then create
+    // a mouse event to click the link to download the file.
+    var blob = new Blob([data], {
+      type: 'text/json'
+    });
+    var e = document.createEvent('MouseEvents');
+    var a = document.createElement('a');
 
     a.download = filename;
     a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
     e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     a.dispatchEvent(e);
- };
+  };
 
 })(console);
